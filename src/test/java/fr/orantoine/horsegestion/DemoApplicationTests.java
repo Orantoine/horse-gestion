6f5c48f41c59911e470e973c4faa3564adebd174
antoine <antoine.godeau@sfr.fr>
2020-05-21 18:16:49 +0200

    :tada: First Commit of the project

---
+++ src/test/java/fr/orantoine/horsegestion/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.orantoine.horsegestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
