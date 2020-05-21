c57273afdff477ebdf2c2adb83aaba81faff0448
antoine <antoine.godeau@sfr.fr>
2020-05-21 18:22:57 +0200

    :sparkles: Add HealthCheck Controller

---
+++ src/main/java/fr/orantoine/horsegestion/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package fr.orantoine.horsegestion.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HealthController {
+
+
+    @GetMapping("/ping")
+    public String healthCheck(){
+        return "pong";
+    }
+
+}
